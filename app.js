ea14293a79f766436e6641501a61522b2eb5f16c
Andrew <kirkmanandrew@aol.com>
2023-02-25 16:20:20 -0500

    Completed the Project

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.get('/', (req, res)=>{
 
 app.use('/stories', storyRoutes);
 
+app.use((req, res, next)=> {
+    let err = new Error('The server cannot locate ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next)=> {
+    console.log(err.stack);
+    if(!err.status) {
+        err.status = 500;
+        err.message = ("Internal Server Error");
+    }
+
+    res.status(err.status);
+    res.render('error', {error: err});
+});
+
 //start the server
 app.listen(port, host, ()=>{
     console.log('hosted at http://localhost:3000');
